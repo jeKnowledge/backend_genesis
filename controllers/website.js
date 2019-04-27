27a205fa44fe2489bc305d78eed4ec3dc2e26632
Miguel Galvão <miguelepgalvao@gmail.com>
2019-04-27 02:52:16 +0100

    neste momento ha uma route /forgot_password que automaticamente envia um mail random controlado por nos, mas e bem provavel que esteja mal coded :( works tho

--- controllers/website.js
+++ controllers/website.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { render_view } from '../utils'
+import mailer  from '../send_email'
 
 let router = express.Router()
 
 router.get('/', (_, res) => {
   render_view(res, 'website/index')
 })
+
+router.use('/forgot_password', mailer)
 
 export default router
