8135a0393a77f676125ff264e49ac14e51900ede
Goncalo Amaral <goa.docs@gmail.com>
2020-01-13 00:09:55 +0000

    Created default response and setup status codes

--- firebase/functions/users/package.js
+++ firebase/functions/users/package.js
@@ -4,25 +4,23 @@ const app = express()
 const cors = require('cors')
 const utils = require('../utils')
 
-const print = utils.print
 const required_params = utils.required_params
+const request_wrapper = utils.request_wrapper
 
 app.use(cors({ origin: true }))
 app.use(express.json())
 
-app.post('/login', (req, res) => {
-  // Has username and password
+app.post('/login', request_wrapper(req => {
+  // WIth username and password
   if (required_params(req.body, ['username', 'password'])) {
-    res.send('HAS USERNAME')
+    return 'Login successful'
   }
-  // Has email and password
+  // With email and password
   else if (required_params(req.body, ['email', 'password'])) {
-    res.send('HAS EMAIL')
+    return 'Login successful'
+  } else {
+    return 'Invalid params expected { username, password } or { email, password }'
   }
-  // Invalid request params
-  else {
-    res.send('INVALID REQUEST')
-  }
-})
+}))
 
 exports.app = functions.https.onRequest(app)
--- firebase/functions/utils.js
+++ firebase/functions/utils.js
@@ -18,6 +18,21 @@ function required_params(params, required_keys) {
   }, true)
 }
 
+function request_wrapper(request_handler) {
+  return (req, res) => {
+    response_payload = request_handler(req, res)
+    if (response_payload == undefined) {
+      res.status(400)
+      res.send("Unhandled request")
+    }
+    else {
+      res.status(200)
+      res.send(response_payload)
+    }
+  }
+}
+
 exports.to_json = to_json
 exports.print = print
 exports.required_params = required_params
+exports.request_wrapper = request_wrapper
